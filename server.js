b50a4f9da6c2ad788d202aa36e07911e75caeeca
IIMRXII <51643169+IIMRXII@users.noreply.github.com>
2024-08-28 00:30:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // Подключаем dotenv
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,17 +6,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Подключение к MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log('Error connecting to MongoDB:', err));
 
-// Обработчик корневого маршрута
+// Подключаем middleware для обработки JSON
+app.use(bodyParser.json());
+
+// Подключаем middleware для обслуживания статических файлов
+app.use(express.static('public')); // Замените 'public' на имя вашей папки, если необходимо
+
+// Обработчик корневого маршрута (по желанию)
 app.get('/', (req, res) => {
     res.send('Привет! Это корень вашего приложения.');
 });
-
-// Middleware
-app.use(bodyParser.json());
 
 // Пример маршрута получения данных пользователя
 app.post('/api/user', async (req, res) => {
